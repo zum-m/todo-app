08ff52b8356859608de806d885e154b52e5bbafd
zum-m <89333894+zum-m@users.noreply.github.com>
2021-10-10 22:48:21 +0900

    div list-rowを生成

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // テキストボックスを所得し初期化
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // divを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
 };
 
 document
